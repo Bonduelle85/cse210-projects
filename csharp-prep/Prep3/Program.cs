8f3e3c3ffca01170bc7a5eb3a76c5e4ba183c91b
Anton Gorokhov <bonduelle85@gmail.com>
2024-01-09 23:22:53 +0500

    C# Prep 3 Stretch Challenge

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -3,7 +3,41 @@ using System;
 class Program
 {
     static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Prep3 World!");
+    {   
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1, 100);
+
+        int userNumber;
+        int counter = 0;
+        string answer = "N";
+
+        do
+        {
+            do
+            {
+                Console.Write("What is your guess? ");
+                string userNumberString = Console.ReadLine();
+                userNumber = int.Parse(userNumberString);
+                counter++;
+
+                if (userNumber > magicNumber)
+                {
+                    Console.WriteLine("Lower");
+                }
+                else if (userNumber < magicNumber)
+                {
+                    Console.WriteLine("Higher");
+                }
+                else
+                {
+                    Console.WriteLine($"You guessed it! Number of attempts: {counter}");
+                    Console.Write("Shall we play again? (Y/N) ");
+                    answer = Console.ReadLine();
+                    counter = 0;
+                }
+            } while (userNumber != magicNumber);
+
+        } while (answer == "Y");
+
     }
 }
