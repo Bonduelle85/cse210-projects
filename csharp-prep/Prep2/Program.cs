23e877374d64461a6a18f7b117b9b478e1c8a1e8
Anton Gorokhov <bonduelle85@gmail.com>
2024-01-09 19:51:44 +0500

    C# Prep 2 Core Requirements

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,42 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage? ");
+        string stringGrade = Console.ReadLine();
+        int intGrade = int.Parse(stringGrade);
+        
+        string letterGrade;
+
+        if (intGrade >= 90)
+        {
+            letterGrade = "A";
+        }
+        else if (intGrade >= 80)
+        {
+            letterGrade = "B";
+        }
+        else if (intGrade >= 70)
+        {
+            letterGrade = "C";
+        }
+        else if (intGrade >= 60)
+        {
+            letterGrade = "D";
+        }
+        else
+        {
+            letterGrade = "F";
+        }
+
+        Console.WriteLine($"\nYour letter grade is {letterGrade}.");
+
+        if (intGrade >= 70)
+        {
+            Console.WriteLine("Congratulations! You passed the test!");
+        }
+        else
+        {
+            Console.WriteLine("You failed the test today. But you will succeed next time.");
+        }
     }
 }
